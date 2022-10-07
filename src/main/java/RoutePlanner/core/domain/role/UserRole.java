c20a4213a706239bd0f6fde026bbc69cb7532ba8
ikk5515 <ikk5515@naver.com>
2022-10-07 18:06:23 +0900

    user 권한 추가

---
+++ src/main/java/RoutePlanner/core/domain/role/UserRole.java
@@ -0,0 +1,14 @@
+package RoutePlanner.core.domain.role;
+
+import lombok.Getter;
+
+@Getter
+public enum UserRole {
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+
+    private String value;
+    UserRole(String value) {
+        this.value = value;
+    }
+}
