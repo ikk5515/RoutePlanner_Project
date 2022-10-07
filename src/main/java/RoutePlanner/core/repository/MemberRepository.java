16f5409f648c4fd5c3ae8d0533155ba2e1debebf
ikk5515 <ikk5515@naver.com>
2022-10-07 18:08:06 +0900

    userID를 통해 로그인하기 위한 기능 추가

--- src/main/java/RoutePlanner/core/repository/MemberRepository.java
+++ src/main/java/RoutePlanner/core/repository/MemberRepository.java
@@ -4,5 +4,10 @@ package RoutePlanner.core.repository;
 import RoutePlanner.core.domain.member.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<SiteUser, Long> {
+    // 사용자 조회 기능
+    Optional<SiteUser> findByUserID(String userID);
+    Optional<SiteUser> findByUserEmail(String userEmail);
 }
