0c77c46928e6f48ef160bbbd188f0e228fc17b2f
ikk5515 <ikk5515@naver.com>
2022-10-07 18:04:32 +0900

    userID와 userPassword 정보 추가

--- src/main/java/RoutePlanner/core/config/SecurityConfig.java
+++ src/main/java/RoutePlanner/core/config/SecurityConfig.java
@@ -1,32 +1,56 @@
 package RoutePlanner.core.config;
 
+import RoutePlanner.core.Service.MemberSecurityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity  //모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만드는 어노테이션
 public class SecurityConfig {
+
+    private final MemberSecurityService memberSecurityService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/**").permitAll()
+        FormLoginConfigurer<HttpSecurity> httpSecurityFormLoginConfigurer = http.formLogin()
+                .usernameParameter("userID")
+                .passwordParameter("userPassword")
+                .loginPage("/Login")
+                .defaultSuccessUrl("/Login");
+
+        LogoutConfigurer<HttpSecurity> httpSecurityLogoutConfigurer = http.logout()   // 로그아웃 기능 작동
+                .logoutUrl("/Logout")
+                .logoutSuccessUrl("/Login");
+
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll()
+
+                // 로그인 URL 등록
                 .and()
-                .csrf().ignoringAntMatchers("/h2-console/**")
-                .and()
-                .headers()
-                .addHeaderWriter(new XFrameOptionsHeaderWriter(
-                        XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
-                ;
+                .csrf().disable()   // 로그인 창
+        ;
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // SpringSecurity 인증 담당
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
