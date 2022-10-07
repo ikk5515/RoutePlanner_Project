f24e6532bfe637edf45845d451127226608d45d2
ikk5515 <ikk5515@naver.com>
2022-10-07 18:08:30 +0900

    일부 유효성 검사 문구 수정

--- src/main/java/RoutePlanner/core/domain/member/Member.java
+++ src/main/java/RoutePlanner/core/domain/member/Member.java
@@ -2,6 +2,7 @@ package RoutePlanner.core.domain.member;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
@@ -12,26 +13,26 @@ public class Member {
     private Long id;
 
     @NotEmpty(message = "아이디는 필수 입력 값입니다.")
-    @Pattern(regexp = "^[a-z0-9]{4,20}$", message = "6~20자의 영문, 숫자만 사용 가능")
+    @Pattern(regexp = "^[a-z0-9]{4,20}$", message = "아이디는 6~20자의 영문, 숫자만 사용 가능")
     private String userID;
 
     @NotEmpty(message = "비밀번호를 입력해주세요")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$", message = "비밀번호는 8~16자리수여야 합니다. 영문 대소문자, 숫자, 특수문자를 1개 이상 포함해야 사용 가능")
-    private String userPassword;
+    private String userPassword1;
 
-    @NotEmpty(message = "비밀번호를 입력해주세요")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$", message = "비밀번호는 8~16자리수여야 합니다. 영문 대소문자, 숫자, 특수문자를 1개 이상 포함해야 사용 가능")
+    @NotEmpty(message = "비밀번호 확인을 입력해주세요")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$", message = "비밀번호 확인은 8~16자리수여야 합니다. 영문 대소문자, 숫자, 특수문자를 1개 이상 포함해야 사용 가능")
     private String userPassword2;
 
 
     @NotEmpty(message = "닉네임은 필수 입력 값입니다.")
-    @Pattern(regexp = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,16}$", message = "2~16자의 영문, 한글, 숫자만 사용 가능")
+    @Pattern(regexp = "^(?=.*[a-z0-9가-힣])[a-z0-9가-힣]{2,16}$", message = "닉네임은 2~16자의 영문, 한글, 숫자만 사용 가능")
     private String userName;
 
     @NotEmpty(message = "이메일은 필수 입력 값입니다.")
     @Email(message = "이메일 형식에 맞지 않습니다.")
     private String userEmail;
 
-    @NotEmpty(message = "성별을 적어주세요.")
+    @NotEmpty(message = "성별을 체크해주세요.")
     private String userGender;
 }
