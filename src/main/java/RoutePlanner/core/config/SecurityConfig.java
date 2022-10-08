872a6f5990c4345f6b171893caf72f6b9dca7bb7
gyojinnK <ksmug1998@gmail.com>
2022-10-09 00:28:34 +0900

    org.springframework.mail.SimpleMailMessage 구문 삭제

--- src/main/java/RoutePlanner/core/config/SecurityConfig.java
+++ src/main/java/RoutePlanner/core/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import RoutePlanner.core.Service.MemberSecurityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
