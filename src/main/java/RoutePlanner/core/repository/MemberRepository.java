7d2f34bf8ff8874935b69a7c0b7477a11678d0fc
ikk5515 <ikk5515@naver.com>
2022-10-09 23:28:26 +0900

    아이디,유저네임,이메일 중복검사 추가

--- src/main/java/RoutePlanner/core/repository/MemberRepository.java
+++ src/main/java/RoutePlanner/core/repository/MemberRepository.java
@@ -9,5 +9,10 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<SiteUser, Long> {
     // 사용자 조회 기능
     Optional<SiteUser> findByUserID(String userID);
-    Optional<SiteUser> findByUserEmail(String userEmail);
+
+    boolean existsByUserID(String userID);
+    boolean existsByUserEmail(String userEmail);
+
+    boolean existsByUserName(String userName);
+
 }
