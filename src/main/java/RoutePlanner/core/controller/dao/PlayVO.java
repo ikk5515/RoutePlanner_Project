8451b608401518279a95835e6b4c164d4b876c17
gyojinnK <ksmug1998@gmail.com>
2022-11-06 04:03:50 +0900

    Play ValueObject 생성

---
+++ src/main/java/RoutePlanner/core/controller/dao/PlayVO.java
@@ -0,0 +1,58 @@
+package RoutePlanner.core.controller.dao;
+
+public class PlayVO {
+    private String playno;
+    private String playkind;
+    private String playwhat;
+    private String playname;
+    private String playaddr;
+    private String playgroup;
+
+    public String getPlayno() {
+        return playno;
+    }
+
+    public String getPlaykind() {
+        return playkind;
+    }
+
+    public String getPlaywhat() {
+        return playwhat;
+    }
+
+    public String getPlayname() {
+        return playname;
+    }
+
+    public String getPlayaddr() {
+        return playaddr;
+    }
+
+    public String getPlaygroup() {
+        return playgroup;
+    }
+
+    public void setPlayno(String playno) {
+        this.playno = playno;
+    }
+
+    public void setPlaykind(String playkind) {
+        this.playkind = playkind;
+    }
+
+    public void setPlaywhat(String playwhat) {
+        this.playwhat = playwhat;
+    }
+
+    public void setPlayname(String playname) {
+        this.playname = playname;
+    }
+
+    public void setPlayaddr(String playaddr) {
+        this.playaddr = playaddr;
+    }
+
+    public void setPlaygroup(String playgroup) {
+        this.playgroup = playgroup;
+    }
+}
