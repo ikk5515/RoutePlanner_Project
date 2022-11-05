24a311b74fe468a30a9d266b1965f349a65f6334
gyojinnK <ksmug1998@gmail.com>
2022-11-06 04:03:25 +0900

    Data Mapping해줄 Controller 생성

---
+++ src/main/java/RoutePlanner/core/controller/dao/DataController.java
@@ -0,0 +1,9 @@
+package RoutePlanner.core.controller.dao;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DataController {
+}
