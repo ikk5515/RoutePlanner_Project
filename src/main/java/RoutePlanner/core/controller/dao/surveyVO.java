7d86c30ed62db73dff61fb94c0463beab6c62793
ikk5515 <ikk5515@naver.com>
2022-11-09 14:58:50 +0900

    userSuvey value Object 선언

---
+++ src/main/java/RoutePlanner/core/controller/dao/surveyVO.java
@@ -0,0 +1,13 @@
+package RoutePlanner.core.controller.dao;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class surveyVO {
+    private String ID;
+    private String userID;
+    private String userWhere;
+    private String userWhat;
+    private String userWho;
+}
