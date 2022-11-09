5732c6ee49edeb0a629e596d6a6a023524cd76e9
gyojinnK <ksmug1998@gmail.com>
2022-11-09 15:54:33 +0900

    userPop 추가 타 VO 객체 주석 처리

--- src/main/java/RoutePlanner/core/controller/HomeController.java
+++ src/main/java/RoutePlanner/core/controller/HomeController.java
@@ -21,13 +21,17 @@ public class HomeController {
     public String mainPage(Model model){
         StringBuilder sb = new StringBuilder();
 
-        DataPopAlgorithm pPop = new DataPopAlgorithm(); //play
-        DataPopAlgorithm teriaPop = new DataPopAlgorithm(); //cafeteria
-        DataPopAlgorithm cPop = new DataPopAlgorithm(); //cafe
+//      DataPopAlgorithm pPop = new DataPopAlgorithm(); //play
+//      DataPopAlgorithm teriaPop = new DataPopAlgorithm(); //cafeteria
+//      DataPopAlgorithm cPop = new DataPopAlgorithm(); //cafe
+//
+//      sb.append(pPop.playPop());
+//      sb.append(teriaPop.cafeteriaPop());
+//      sb.append(cPop.cafePop());
 
-        sb.append(pPop.playPop());
-        sb.append(teriaPop.cafeteriaPop());
-        sb.append(cPop.cafePop());
+        DataPopAlgorithm uPop = new DataPopAlgorithm();
+
+        sb.append(uPop.userPop());
 
         model.addAttribute("data", sb);
         return "/main";
